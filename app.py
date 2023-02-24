99808fd376706735465dfe85e69d40f654bfa7e5
pjhu6 <pjhu@uci.edu>
2023-02-24 02:45:36 -0800

    basic next/back buttons

--- app.py
+++ app.py
@@ -1,26 +1,35 @@
 import PySimpleGUI as sg
 
-layouts = {'Home': 1, 'Pet Q1': 2, "Pet Q2": 3}
+layouts = {'Phone Number', 'Home', 'Pet Q1', "Pet Q2"}
 
-layout1 = [[sg.Text('Home page')]]
+phone_page = [[sg.Text('Enter your phone number')],
+            [sg.Button('Next'), sg.Button('Exit')]]
 
-layout2 = [[sg.Text('Pet question 1')]]
+layout1 = [[sg.Text('Home page')], 
+        [sg.Button('Back'), sg.Button('Next'), sg.Button('Exit')]]
 
-layout3 = [[sg.Text('Pet question 2')]]
+layout2 = [[sg.Text('Pet question 1')],
+        [sg.Button('Back'), sg.Button('Next'), sg.Button('Exit')]]
 
-layout = [[sg.Column(layout1, key='1'), sg.Column(layout2, visible=False, key='2'), sg.Column(layout3, visible=False, key='3')],
-          [sg.Button('Home'), sg.Button('Pet Q1'), sg.Button('Pet Q2'), sg.Button('Exit')]]
+layout3 = [[sg.Text('Pet question 2')],
+        [sg.Button('Back'), sg.Button('Next'), sg.Button('Exit')]]
+
+layout = [[sg.Column(phone_page, key='1'), sg.Column(layout1, visible=False, key='2'), sg.Column(layout2, visible=False, key='3'), sg.Column(layout3, visible=False, key='4')]]
 
 window = sg.Window('Swapping the contents of a window', layout)
 
-layout_num = 1  # The currently visible layout
+layout_num = 1 # The currently visible layout
 while True:
     event, values = window.read()
     print(event, values)
     if event in (None, 'Exit'):
         break
-    elif event in layouts.keys():
+    elif "Next" in event:
         window[f'{layout_num}'].update(visible=False)
-        layout_num = int(layouts[event])
+        layout_num += 1
+        window[f'{layout_num }'].update(visible=True)
+    elif "Back" in event:
+        window[f'{layout_num}'].update(visible=False)
+        layout_num -= 1
         window[f'{layout_num }'].update(visible=True)
 window.close()
